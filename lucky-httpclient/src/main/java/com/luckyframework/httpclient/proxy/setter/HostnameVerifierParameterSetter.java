24068989f67efbc04eafba2f6050957982fbf895
fukang1163 <fukang1163@cairenhui.com>
2024-02-06 17:41:34 +0800

    20240206 新增SSL相关的参数设置器；部分日志打印优化

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/setter/HostnameVerifierParameterSetter.java
@@ -0,0 +1,22 @@
+package com.luckyframework.httpclient.proxy.setter;
+
+
+import com.luckyframework.httpclient.core.Request;
+import com.luckyframework.httpclient.proxy.ParamInfo;
+
+import javax.net.ssl.HostnameVerifier;
+
+/**
+ * HostnameVerifier 参数设置器
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/25 10:21
+ */
+public class HostnameVerifierParameterSetter implements ParameterSetter {
+
+    @Override
+    public void set(Request request, ParamInfo paramInfo) {
+        request.setHostnameVerifier(((HostnameVerifier) paramInfo.getValue()));
+    }
+}
---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/setter/SSLSocketFactoryParameterSetter.java
@@ -0,0 +1,20 @@
+package com.luckyframework.httpclient.proxy.setter;
+
+import com.luckyframework.httpclient.core.Request;
+import com.luckyframework.httpclient.proxy.ParamInfo;
+
+import javax.net.ssl.SSLSocketFactory;
+
+/**
+ * SSLSocketFactory 参数设置器
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/25 10:21
+ */
+public class SSLSocketFactoryParameterSetter implements ParameterSetter {
+    @Override
+    public void set(Request request, ParamInfo paramInfo) {
+        request.setSSLSocketFactory((SSLSocketFactory) paramInfo.getValue());
+    }
+}
