f02763c2328b5015d628342639042051616c31bb
fukang1163 <fukang1163@cairenhui.com>
2024-02-26 15:15:49 +0800

    20240226 准备HTTPS相关的封装

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/core/https/TrustAllHostnameVerifier.java
@@ -0,0 +1,21 @@
+package com.luckyframework.httpclient.core.https;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+/**
+ * 信任所有域名的域名验证器
+ *
+ * @author fk7075
+ * @version 1.0
+ * @date 2024/2/26 14:56
+ */
+public class TrustAllHostnameVerifier implements HostnameVerifier {
+
+    public static final HostnameVerifier DEFAULT_INSTANCE = new TrustAllHostnameVerifier();
+
+    @Override
+    public boolean verify(String s, SSLSession sslSession) {
+        return true;
+    }
+}
