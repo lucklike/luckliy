e9ab59a0a1adcfaa4a5fb0a467811baac7d6472b
fk7075 <1814375626@qq.com>
2024-03-14 01:24:21 +0800

    20240314 新增Multi系列注解

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/MultiFileParam.java
@@ -0,0 +1,42 @@
+package com.luckyframework.httpclient.proxy.annotations;
+
+import com.luckyframework.httpclient.proxy.dynamic.MultiFileDynamicParamResolver;
+import com.luckyframework.httpclient.proxy.setter.StandardHttpFileParameterSetter;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * multipart/form-data参数注解
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/25 12:25
+ */
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@DynamicParam(
+        setter = @ObjectGenerate(StandardHttpFileParameterSetter.class),
+        resolver = @ObjectGenerate(MultiFileDynamicParamResolver.class)
+)
+public @interface MultiFileParam {
+
+    /**
+     * 参数名称
+     */
+    @AliasFor(annotation = DynamicParam.class, attribute = "name")
+    String value() default "";
+
+    @AliasFor(annotation = DynamicParam.class, attribute = "name")
+    String name() default "";
+
+    String fileName() default "";
+
+}
