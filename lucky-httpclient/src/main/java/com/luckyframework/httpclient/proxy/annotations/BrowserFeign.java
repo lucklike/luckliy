e563bf07926beb4be4db4eb3dcf998d7b426347e
fukang1163 <fukang1163@cairenhui.com>
2024-04-07 14:21:42 +0800

    20240407 新增@BrowserFeign注解，用于将请求伪装成浏览器发出的，原理是设置User-Agent

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/BrowserFeign.java
@@ -0,0 +1,37 @@
+package com.luckyframework.httpclient.proxy.annotations;
+
+import com.luckyframework.reflect.Combination;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 浏览器伪装
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/30 02:46
+ */
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@StaticHeader("[SET]User-Agent=#{$ann$.userAgent}")
+@Combination(StaticHeader.class)
+public @interface BrowserFeign {
+
+    String CHROME = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
+    String EDG = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0";
+    String FIREFOX = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+    String OPERA = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41";
+    String SAFARI = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1";
+    String IE = "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)";
+    String BOT = "Googlebot/2.1 (+http://www.google.com/bot.html)";
+
+
+    String userAgent() default CHROME;
+}
