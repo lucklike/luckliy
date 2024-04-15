d7e5d29c3afa32ee67758bcd729f784b241ae002
fukang1163 <fukang1163@cairenhui.com>
2024-04-15 14:41:59 +0800

    20240415 不再推荐使用@SpElSelect注解，建议使用它的平替注解@ResponseSelect

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/convert/SpELResponseSelectConvert.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/convert/SpELResponseSelectConvert.java
@@ -2,7 +2,7 @@ package com.luckyframework.httpclient.proxy.convert;
 
 import com.luckyframework.common.StringUtils;
 import com.luckyframework.httpclient.core.Response;
-import com.luckyframework.httpclient.proxy.annotations.SpElSelect;
+import com.luckyframework.httpclient.proxy.annotations.ResponseSelect;
 
 /**
  * 基于SpEL表达式的返回值结果转换器
@@ -16,7 +16,7 @@ public class SpELResponseSelectConvert extends AbstractSpELResponseConvert {
     @Override
     public <T> T convert(Response response, ConvertContext context) {
         // 获取表达式配置
-        String expression = context.toAnnotation(SpElSelect.class).expression();
+        String expression = context.toAnnotation(ResponseSelect.class).expression();
 
         // 没有做任何配置时，直接对返回值进行转换
         if (!StringUtils.hasText(expression)) {
