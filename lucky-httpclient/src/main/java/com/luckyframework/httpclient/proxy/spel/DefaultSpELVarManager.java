8cabc3ff8852cdd68a888c69ebac925b37fab0d4
fukang1163 <fukang1163@cairenhui.com>
2024-06-11 17:51:34 +0800

    20240611 【回退】响应码不为2XX或者3XX时不会写入SpEL上下文

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/spel/DefaultSpELVarManager.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/spel/DefaultSpELVarManager.java
@@ -90,7 +90,7 @@ public abstract class DefaultSpELVarManager implements SpELVarManager {
             responseVar.addRootVariable(RESPONSE_HEADER, response.getSimpleHeaders());
             responseVar.addRootVariable(RESPONSE_COOKIE, response.getSimpleCookies());
         }
-        if (importAnn.importBody() && response.getResponseMetaData().isSuccess()) {
+        if (importAnn.importBody()) {
             responseVar.addRootVariable(RESPONSE_BODY, getResponseBody(response, context.getConvertMetaType()));
         }
     }
