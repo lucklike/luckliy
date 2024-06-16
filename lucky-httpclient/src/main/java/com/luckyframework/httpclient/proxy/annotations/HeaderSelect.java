4ff2006e6b307f86049d9c001aa990c67012c6e9
fk7075 <1814375626@qq.com>
2024-06-16 08:47:46 +0800

    20240616 新增@CookieSelect、@HeaderSelect注解，可以很方便的获取Cookie信息和Header信息

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/HeaderSelect.java
@@ -0,0 +1,221 @@
+package com.luckyframework.httpclient.proxy.annotations;
+
+import com.luckyframework.httpclient.proxy.TAG;
+import com.luckyframework.httpclient.proxy.convert.HeaderResponseConvert;
+import com.luckyframework.reflect.Combination;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 支持SpEL表达式的响应头选择器
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/25 12:25
+ */
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@NotAnalyzeBody
+@Combination(ResultConvert.class)
+@ResultConvert(
+        convert = @ObjectGenerate(HeaderResponseConvert.class),
+        importBody = false,
+        importHeader = false
+)
+public @interface HeaderSelect {
+
+    /**
+     * <pre>
+     *     对响应结果进行操作的SpEL表达式，<b>SpEL表达式部分需要写在#{}中</b>
+     *     1.<a href="https://docs.spring.io/spring-framework/reference/core/expressions.htm">Spring Expression Language Official Document</a>
+     *
+     *     2.Elvis运算符
+     *     {@code
+     *          x?:y
+     *          ->
+     *          x != null ? x : y
+     *     }
+     *
+     *     3.安全导航操作符（避免空指针异常）
+     *     {@code
+     *          object?.field
+     *          ->
+     *          当object为null时会直接返回null（不产生空指针异常），不为null时返回object的field属性
+     *     }
+     *
+     *     4.集合选择器语法，对集合或Map进行过滤
+     *       {@code
+     *          [collection | array | map].?[selectorExpression]
+     *          扩展：.^[]选取满足要求的第一个元素， .$[]选取满足要求的最后一个
+     *          eg:
+     *          a.  int[] array = {1,2,3,4,5,6,7,8,9,10}
+     *              array.?[#this % 2 == 0]
+     *                     ->
+     *              {2,4,6,8,10}
+     *
+     *          b. Map map = {a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10,k=11,l=12}
+     *             map.?[value > 5 && (key eq 'a' || key eq 'f' || key eq 'j')]
+     *                     ->
+     *             {f=6, j=10}
+     *       }
+     *     5.集合投影语法，对集合或Map的元素进行操作，生成一个新集合
+     *       {@code
+     *          [collection | array | map].![expression]
+     *          eg:
+     *           a.  int[] array = {1,2,3,4,5,6,7,8,9,10}
+     *               array.![#this + 1]
+     *                      ->
+     *               {2,3,4,5,6,7,8,9,10,11}
+     *
+     *           b.  Map map = {a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9,j=10,k=11,l=12}
+     *               map.![value+1]
+     *                      ->
+     *               [2,3,4,5,6,7,8,9,10,11,12,13]
+     *
+     *           c. List<Map> list = [{a=1,b=2,c=3}]
+     *              list.![{'A':a, 'B':b, 'C':c}]
+     *                      ->
+     *              [{A=1,B=2,C=3}]
+     *       }
+     *
+     * SpEL表达式内置参数有：
+     * root: {
+     *      <b>SpEL Env : </b>
+     *      {@value TAG#SPRING_ROOT_VAL}
+     *      {@value TAG#SPRING_VAL}
+     *
+     *      <b>Context : </b>
+     *      {@value TAG#METHOD_CONTEXT}
+     *      {@value TAG#CLASS_CONTEXT}
+     *      {@value TAG#ANNOTATION_CONTEXT}
+     *      {@value TAG#CLASS}
+     *      {@value TAG#METHOD}
+     *      {@value TAG#THIS}
+     *      {@value TAG#ANNOTATION_INSTANCE}
+     *      {@value TAG#PARAM_TYPE}
+     *      {@value TAG#PN}
+     *      {@value TAG#PN_TYPE}
+     *      {@value TAG#PARAM_NAME}
+     *
+     *      <b>Request : </b>
+     *      {@value TAG#REQUEST}
+     *      {@value TAG#REQUEST_URL}
+     *      {@value TAG#REQUEST_METHOD}
+     *      {@value TAG#REQUEST_QUERY}
+     *      {@value TAG#REQUEST_PATH}
+     *      {@value TAG#REQUEST_FORM}
+     *      {@value TAG#REQUEST_HEADER}
+     *      {@value TAG#REQUEST_COOKIE}
+     *
+     *      <b>Response : </b>
+     *      {@value TAG#RESPONSE}
+     *      {@value TAG#RESPONSE_STATUS}
+     *      {@value TAG#CONTENT_LENGTH}
+     *      {@value TAG#CONTENT_TYPE}
+     *      <b>所有Header信息，取值表达式必须为全大写</b>
+     * }
+     * </pre>
+     *
+     */
+    @AliasFor("expression")
+    String value() default "";
+
+    /**
+     * 同value
+     */
+    @AliasFor("value")
+    String expression() default "";
+
+    /**
+     * 当取值表达式取不到值时可以通过这个属性来设置默认值，
+     * 这里允许使用SpEL表达式来生成一个默认值，<b>SpEL表达式部分需要写在#{}中</b>
+     *
+     * <pre>
+     * SpEL表达式内置参数有：
+     * root: {
+     *      <b>SpEL Env : </b>
+     *      {@value TAG#SPRING_ROOT_VAL}
+     *      {@value TAG#SPRING_VAL}
+     *
+     *      <b>Context : </b>
+     *      {@value TAG#METHOD_CONTEXT}
+     *      {@value TAG#CLASS_CONTEXT}
+     *      {@value TAG#ANNOTATION_CONTEXT}
+     *      {@value TAG#CLASS}
+     *      {@value TAG#METHOD}
+     *      {@value TAG#THIS}
+     *      {@value TAG#ANNOTATION_INSTANCE}
+     *      {@value TAG#PARAM_TYPE}
+     *      {@value TAG#PN}
+     *      {@value TAG#PN_TYPE}
+     *      {@value TAG#PARAM_NAME}
+     *
+     *      <b>Request : </b>
+     *      {@value TAG#REQUEST}
+     *      {@value TAG#REQUEST_URL}
+     *      {@value TAG#REQUEST_METHOD}
+     *      {@value TAG#REQUEST_QUERY}
+     *      {@value TAG#REQUEST_PATH}
+     *      {@value TAG#REQUEST_FORM}
+     *      {@value TAG#REQUEST_HEADER}
+     *      {@value TAG#REQUEST_COOKIE}
+     *
+     *      <b>Response : </b>
+     *      {@value TAG#RESPONSE}
+     * }
+     * </pre>
+     */
+    @AliasFor(annotation = ResultConvert.class, attribute = "defaultValue")
+    String defaultValue() default "";
+
+    /**
+     * 异常信息，当从条件表达式中无法获取值时又没有设置默认值时
+     * 配置了该属性则会抛出携带该异常信息的异常，
+     * 这里允许使用SpEL表达式来生成一个默认值，<b>SpEL表达式部分需要写在#{}中</b>
+     *
+     * <pre>
+     * SpEL表达式内置参数有：
+     * root: {
+     *      <b>SpEL Env : </b>
+     *      {@value TAG#SPRING_ROOT_VAL}
+     *      {@value TAG#SPRING_VAL}
+     *
+     *      <b>Context : </b>
+     *      {@value TAG#METHOD_CONTEXT}
+     *      {@value TAG#CLASS_CONTEXT}
+     *      {@value TAG#ANNOTATION_CONTEXT}
+     *      {@value TAG#CLASS}
+     *      {@value TAG#METHOD}
+     *      {@value TAG#THIS}
+     *      {@value TAG#ANNOTATION_INSTANCE}
+     *      {@value TAG#PARAM_TYPE}
+     *      {@value TAG#PN}
+     *      {@value TAG#PN_TYPE}
+     *      {@value TAG#PARAM_NAME}
+     *
+     *      <b>Request : </b>
+     *      {@value TAG#REQUEST}
+     *      {@value TAG#REQUEST_URL}
+     *      {@value TAG#REQUEST_METHOD}
+     *      {@value TAG#REQUEST_QUERY}
+     *      {@value TAG#REQUEST_PATH}
+     *      {@value TAG#REQUEST_FORM}
+     *      {@value TAG#REQUEST_HEADER}
+     *      {@value TAG#REQUEST_COOKIE}
+     *
+     *      <b>Response : </b>
+     *      {@value TAG#RESPONSE}
+     * }
+     * </pre>
+     */
+    @AliasFor(annotation = ResultConvert.class, attribute = "exception")
+    String exception() default "";
+}
---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/convert/CookieResponseConvert.java
@@ -0,0 +1,36 @@
+package com.luckyframework.httpclient.proxy.convert;
+
+import com.luckyframework.common.StringUtils;
+import com.luckyframework.httpclient.core.Response;
+import com.luckyframework.httpclient.proxy.annotations.CookieSelect;
+
+import static com.luckyframework.httpclient.proxy.ParameterNameConstant.CONTENT_LENGTH;
+import static com.luckyframework.httpclient.proxy.ParameterNameConstant.CONTENT_TYPE;
+import static com.luckyframework.httpclient.proxy.ParameterNameConstant.RESPONSE_STATUS;
+
+/**
+ * Cookie转换器
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2024/6/16 07:54
+ */
+public class CookieResponseConvert extends AbstractSpELResponseConvert {
+
+    @Override
+    public <T> T convert(Response response, ConvertContext context) throws Throwable {
+        CookieSelect ann = context.toAnnotation(CookieSelect.class);
+        String expression = ann.expression();
+        if (!StringUtils.hasText(expression)) {
+            throw new IllegalArgumentException("The 'expression' property of the @CookieSelect annotation cannot be empty.");
+        }
+        T returnObject = context.parseExpression(expression, context.getRealMethodReturnType(), pws -> {
+            pws.addRootVariables(response.getSimpleCookies());
+            pws.addRootVariable(RESPONSE_STATUS, response.getStatus());
+            pws.addRootVariable(CONTENT_LENGTH, response.getContentLength());
+            pws.addRootVariable(CONTENT_TYPE, response.getContentType());
+        });
+        return returnObject != null ? returnObject : getDefaultValue(context);
+    }
+
+}
