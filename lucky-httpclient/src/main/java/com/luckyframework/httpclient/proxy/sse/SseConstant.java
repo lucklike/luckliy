574e7b0354b385b330a146a01ef3fb31b8db1fc0
fukang1163 <fukang1163@cairenhui.com>
2024-07-26 18:30:07 +0800

    20240726 SSEAPI优化

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/sse/SseConstant.java
@@ -0,0 +1,30 @@
+package com.luckyframework.httpclient.proxy.sse;
+
+public class SseConstant {
+
+    /**
+     * 消息ID
+     */
+    public static final String ID = "id";
+
+    /**
+     * 事件类型
+     */
+    public static final String EVENT = "event";
+
+    /**
+     * 消息数据
+     */
+    public static final String DATA = "data";
+
+    /**
+     * 重试相关的额设置
+     */
+    public static final String RETRY = "retry";
+
+    /**
+     * 注释
+     */
+    public static final String COMMENT = "";
+
+}
