7f7664a948b23f8c25972c17eb54a3366ce79783
fukang1163 <fukang1163@cairenhui.com>
2024-08-12 18:07:27 +0800

    20240812 SSE的Message对象新增注释

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/sse/Message.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/sse/Message.java
@@ -1,5 +1,6 @@
 package com.luckyframework.httpclient.proxy.sse;
 
+import com.luckyframework.common.ConfigurationMap;
 import com.luckyframework.common.StringUtils;
 import com.luckyframework.serializable.SerializationException;
 import com.luckyframework.serializable.SerializationTypeToken;
@@ -26,7 +27,7 @@ public class Message {
     private final String id;
 
     /**
-     * 事件类型
+     * 消息类型
      */
     private final String event;
 
@@ -36,7 +37,7 @@ public class Message {
     private final String data;
 
     /**
-     * 重试相关的额设置
+     * 重试相关的设置
      */
     private final String retry;
 
@@ -59,62 +60,143 @@ public class Message {
         this.comment = msgProperties.getProperty(COMMENT);
     }
 
+    /**
+     * 获取消息ID
+     *
+     * @return 消息ID
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * 获取消息类型
+     *
+     * @return 消息类型
+     */
     public String getEvent() {
         return event;
     }
 
+    /**
+     * 获取消息数据
+     *
+     * @return 消息数据
+     */
     public String getData() {
         return data;
     }
 
+    /**
+     * 获取重试信息
+     *
+     * @return 重试信息
+     */
     public String getRetry() {
         return retry;
     }
 
+    /**
+     * 获取注释说明
+     *
+     * @return 注释说明
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * 获取所有消息数据
+     *
+     * @return 所有消息数据
+     */
     public Properties getMsgProperties() {
         return msgProperties;
     }
 
+    /**
+     * 根据指定的key或者对应的消息
+     *
+     * @param key Key名称
+     * @return 对应的消息数据
+     */
     public String getProperty(String key) {
         return msgProperties.getProperty(key);
     }
 
+    /**
+     * 根据指定的key或者对应的消息，如果获取不到则返回默认值
+     *
+     * @param key          Key名称
+     * @param defaultValue 默认值
+     * @return 对应的消息数据
+     */
     public String getPropertyOrDefault(String key, String defaultValue) {
         return hasProperty(key) ? getProperty(key) : defaultValue;
     }
 
+    /**
+     * 判断名称为Key的消息是否存在
+     *
+     * @param key Key名称
+     * @return 名称为Key的消息是否存在
+     */
     public boolean hasProperty(String key) {
         return msgProperties.containsKey(key);
     }
 
+    /**
+     * 是否存在消息ID
+     *
+     * @return 是否存在消息ID
+     */
     public boolean hasId() {
         return hasProperty(ID);
     }
 
+    /**
+     * 是否存在消息类型
+     *
+     * @return 是否存在消息类型
+     */
     public boolean hasEvent() {
         return hasProperty(EVENT);
     }
 
+    /**
+     * 是否存在消息数据
+     *
+     * @return 是否存在消息数据
+     */
     public boolean hasData() {
         return hasProperty(DATA);
     }
 
+    /**
+     * 是否存在重试相关信息
+     *
+     * @return 是否存在重试相关信息
+     */
     public boolean hasRetry() {
         return hasProperty(RETRY);
     }
 
+    /**
+     * 是否存在消息的注释信息
+     *
+     * @return 是否存在消息的注释信息
+     */
     public boolean hasComment() {
         return hasProperty(COMMENT);
     }
 
+    /**
+     * 将JSON格式的消息数据转化为Java对象
+     *
+     * @param objectType Java对象类型
+     * @param <T>        Java对象类型
+     * @return Java对象
+     */
     public <T> T jsonDataToEntity(Type objectType) {
         if (!StringUtils.hasText(data)) {
             return null;
@@ -126,10 +208,32 @@ public class Message {
         }
     }
 
+    /**
+     * 将JSON格式的消息数据转化为复杂的Java对象
+     *
+     * @param token Java对象类型token
+     * @param <T>   Java对象类型
+     * @return Java对象
+     */
     public <T> T jsonDataToEntity(SerializationTypeToken<T> token) {
         return jsonDataToEntity(token.getType());
     }
 
+    /**
+     * 将JSON格式的消息数据转化为{@link ConfigurationMap}对象
+     * @return ConfigurationMap对象
+     */
+    public ConfigurationMap jsonDataToMap() {
+        return javaDataToEntity(ConfigurationMap.class);
+    }
+
+    /**
+     * 将XML格式的消息数据转化为Java对象
+     *
+     * @param objectType Java对象类型
+     * @param <T>        Java对象类型
+     * @return Java对象
+     */
     public <T> T xmlDataToEntity(Type objectType) {
         if (!StringUtils.hasText(data)) {
             return null;
@@ -141,10 +245,24 @@ public class Message {
         }
     }
 
+    /**
+     * 将XML格式的消息数据转化为复杂的Java对象
+     *
+     * @param token Java对象类型token
+     * @param <T>   Java对象类型
+     * @return Java对象
+     */
     public <T> T xmlDataToEntity(SerializationTypeToken<T> token) {
         return xmlDataToEntity(token.getType());
     }
 
+    /**
+     * 将JDK序列化后的消息数据转化为Java对象
+     *
+     * @param objectType Java对象类型
+     * @param <T>        Java对象类型
+     * @return Java对象
+     */
     public <T> T javaDataToEntity(Type objectType) {
         if (!StringUtils.hasText(data)) {
             return null;
@@ -156,6 +274,13 @@ public class Message {
         }
     }
 
+    /**
+     * 将JDK序列化后的消息数据转化为复杂Java对象
+     *
+     * @param objectType Java对象类型token
+     * @param <T>        Java对象类型
+     * @return Java对象
+     */
     public <T> T javaDataToEntity(SerializationTypeToken<T> token) {
         return javaDataToEntity(token.getType());
     }
