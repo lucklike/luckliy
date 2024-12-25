69a92f1cac6cdb0b248713f77360898b3f93650f
fk7075 <1814375626@qq.com>
2024-12-26 02:22:06 +0800

    20241226 新增两个代理注解@HttpProxy、@SocksProxy

---
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/SocksProxy.java
@@ -0,0 +1,86 @@
+package com.luckyframework.httpclient.proxy.annotations;
+
+import com.luckyframework.httpclient.proxy.TAG;
+import com.luckyframework.reflect.Combination;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.net.Proxy;
+
+/**
+ * 代理参数配置注解
+ *
+ * @author fukang
+ * @version 1.0.0
+ * @date 2023/7/30 02:46
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@UseProxy(type = Proxy.Type.SOCKS, ip = "", port = "")
+@Combination({UseProxy.class})
+public @interface SocksProxy {
+
+    /**
+     * IP,支持SpEL表达式，SpEL表达式部分需要写在#{}中
+     * <pre>
+     * SpEL表达式内置参数有：
+     *  root:{
+     *      <b>SpEL Env : </b>
+     *      {@value TAG#SPRING_ROOT_VAL}
+     *      {@value TAG#SPRING_VAL}
+     *
+     *      <b>Context : </b>
+     *      {@value TAG#METHOD_CONTEXT}
+     *      {@value TAG#CLASS_CONTEXT}
+     *      {@value TAG#ANNOTATION_CONTEXT}
+     *      {@value TAG#CLASS}
+     *      {@value TAG#METHOD}
+     *      {@value TAG#THIS}
+     *      {@value TAG#PARAM_TYPE}
+     *      {@value TAG#PN}
+     *      {@value TAG#PN_TYPE}
+     *      {@value TAG#PARAM_NAME}
+     *  }
+     * </pre>
+     */
+    @AliasFor(annotation = UseProxy.class, attribute = "ip")
+    String ip();
+
+    /**
+     * 端口,支持SpEL表达式，SpEL表达式部分需要写在#{}中
+     * <pre>
+     * SpEL表达式内置参数有：
+     *  root:{
+     *      <b>SpEL Env : </b>
+     *      {@value TAG#SPRING_ROOT_VAL}
+     *      {@value TAG#SPRING_VAL}
+     *
+     *      <b>Context : </b>
+     *      {@value TAG#METHOD_CONTEXT}
+     *      {@value TAG#CLASS_CONTEXT}
+     *      {@value TAG#ANNOTATION_CONTEXT}
+     *      {@value TAG#CLASS}
+     *      {@value TAG#METHOD}
+     *      {@value TAG#THIS}
+     *      {@value TAG#PARAM_TYPE}
+     *      {@value TAG#PN}
+     *      {@value TAG#PN_TYPE}
+     *      {@value TAG#PARAM_NAME}
+     *  }
+     * </pre>
+     */
+    @AliasFor(annotation = UseProxy.class, attribute = "port")
+    String port();
+
+    String username() default "";
+
+    String password() default "";
+
+}
