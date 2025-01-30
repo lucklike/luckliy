9b6aac90ec8f998cfda2714bf3d808141919e47b
fk7075 <1814375626@qq.com>
2025-01-30 23:04:16 +0800

    202501230 最后一块拼图-插件机制（类似动态代理机制）

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/plugin/ProxyPlugin.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/plugin/ProxyPlugin.java
@@ -1,7 +1,5 @@
 package com.luckyframework.httpclient.proxy.plugin;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 /**
  * 代理插件
  *
@@ -11,11 +9,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 @FunctionalInterface
 public interface ProxyPlugin {
-
-    /**
-     * 初始化标志
-     */
-    AtomicBoolean initialized = new AtomicBoolean(false);
 
     /**
      * 以插件的方式运行当前正在执行的方法<br/>
@@ -53,17 +46,6 @@ public interface ProxyPlugin {
      * @param meta 执行元数据
      */
     default void init(ExecuteMeta meta) {
-        if (initialized.compareAndSet(false, true)) {
-            initOneOnly(meta);
-        }
-    }
-
-    /**
-     * 只执行一次的初始化方法
-     *
-     * @param meta 执行元数据
-     */
-    default void initOneOnly(ExecuteMeta meta) {
 
     }
 
