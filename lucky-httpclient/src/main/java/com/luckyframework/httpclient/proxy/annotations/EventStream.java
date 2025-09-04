3af5b4b0e72b10d0d00e2f9c9dfa0c47d8a92a8f
fukang1163 <fukang1163@cairenhui.com>
2025-09-04 16:51:44 +0800

    20250904 尝试将SSE封装为Flux

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/EventStream.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/annotations/EventStream.java
@@ -1,5 +1,8 @@
 package com.luckyframework.httpclient.proxy.annotations;
 
+import com.luckyframework.reflect.Combination;
+import org.springframework.core.annotation.AliasFor;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -18,8 +21,46 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
+@Timeout
 @AutoCloseResponse(false)
 @RespConvert("#{$resp$}")
+@Combination({RespConvert.class, Timeout.class})
 public @interface EventStream {
+
+    /**
+     * 连接超时时间
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "connectionTimeout")
+    int connectionTimeout() default -1;
+
+    /**
+     * 连接超时时间的SpEL表达式，SpEL表达式部分需要写在#{}中
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "connectionTimeoutExp")
+    String connectionTimeoutExp() default "";
+
+    /**
+     * 读取超时时间
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "readTimeout")
+    int readTimeout() default 600000;
+
+    /**
+     * 读取超时时间的SpEL表达式，SpEL表达式部分需要写在#{}中
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "readTimeoutExp")
+    String readTimeoutExp() default "";
+
+    /**
+     * 写超时时间
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "writeTimeout")
+    int writeTimeout() default -1;
+
+    /**
+     * 写超时时间的SpEL表达式，SpEL表达式部分需要写在#{}中
+     */
+    @AliasFor(annotation = Timeout.class, attribute = "writeTimeoutExp")
+    String writeTimeoutExp() default "";
 
 }
