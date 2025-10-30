2813cefc6e1c6d3c337e6e9e0f6adc57afe04301
fukang1163 <fukang1163@cairenhui.com>
2025-10-30 17:41:01 +0800

    20251030 注释优化

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/retry/RunBeforeRetryContext.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/retry/RunBeforeRetryContext.java
@@ -3,8 +3,10 @@ package com.luckyframework.httpclient.proxy.retry;
 import com.luckyframework.httpclient.proxy.spel.hook.Lifecycle;
 import com.luckyframework.retry.RunBeforeRetry;
 import com.luckyframework.retry.TaskResult;
+import com.luckyframework.spel.LazyValue;
 
 import static com.luckyframework.httpclient.proxy.spel.OrdinaryVarName._$RETRY_COUNT$_;
+import static com.luckyframework.httpclient.proxy.spel.OrdinaryVarName._$TASK_RESULT$_;
 
 /**
  * 携带上下文的重试执行器
@@ -17,8 +19,9 @@ public abstract class RunBeforeRetryContext<T> extends RetryContext implements R
 
     @Override
     public void beforeRetry(TaskResult<T> taskResult) {
-        getContext().useHook(Lifecycle.RETRY);
+        getContextVar().addRootVariable(_$TASK_RESULT$_, taskResult);
         getContextVar().addRootVariable(_$RETRY_COUNT$_, taskResult.getRetryNum());
+        getContext().useHook(Lifecycle.RETRY);
         doBeforeRetry(taskResult);
     }
 
