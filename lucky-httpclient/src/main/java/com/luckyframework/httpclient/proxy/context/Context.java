81f4f2a05dbc218ce109f9f45c1b02646bc61d74
fukang1163 <fukang1163@cairenhui.com>
2025-11-18 19:59:58 +0800

    20251118 方法名优化

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/context/Context.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/context/Context.java
@@ -974,7 +974,7 @@ public abstract class Context implements ContextSpELExecution {
                                                                               Function<FnuExceptionWrap, E> targetFuncException) {
 
         if (!ClassUtils.compatibleOrNot(returnType, ResolvableType.forMethodReturnType(func))) {
-            throw new FunctionReturnTypeNonMatchException("Function return type '{}' is not compatible with target type '{}'", returnType, returnType);
+            throw new FunctionReturnTypeNonMatchException("Function return type '{}' is not compatible with target type '{}'", ResolvableType.forMethodReturnType(func), returnType);
         }
 
         try {
