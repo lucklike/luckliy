cccca6dba951ecef5f747bf53b9e8753de36e8c4
fukang1163 <fukang1163@cairenhui.com>
2025-12-22 09:50:29 +0800

    20251222 不打印logo

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/Version.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/Version.java
@@ -1,16 +1,11 @@
 package com.luckyframework.httpclient.proxy;
 
-import com.luckyframework.common.Console;
 import com.luckyframework.common.FontUtil;
 import com.luckyframework.common.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Optional;
-
-import static com.luckyframework.httpclient.proxy.function.ResourceFunctions.read;
-import static com.luckyframework.httpclient.proxy.function.ResourceFunctions.resource;
 
 
 /**
