7642a2094fd39bffea679924d9fe0c3969454ed8
fk7075 <1814375626@qq.com>
2025-12-22 23:54:14 +0800

    20251223 换 logo

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/Version.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/Version.java
@@ -18,7 +18,6 @@ public class Version {
     public static final String LUCKY_VERSION = getLuckyHttpClientVersion();
     public static final String JAVA_VERSION = System.getProperty("java.version");
     public static final String LUCKY_USER_AGENT = StringUtils.format("Lucky-HttpClient/{} (Java/{})", LUCKY_VERSION, JAVA_VERSION);
-    private static final String LUCKY_LOGO_FILE = "classpath:lucky-httpclient.logo";
 
     /**
      * 获取当前lucky-httpclient版本号
@@ -33,17 +32,7 @@ public class Version {
      * 打印版本信息
      */
     public static void printVersion() {
-        logger.info(FontUtil.getGreenUnderline(StringUtils.format("lucky-httpclient-{}", LUCKY_VERSION)));
+        logger.info("🍀 {} 🍀",FontUtil.getGreenUnderline(StringUtils.format("lucky-httpclient-{}", LUCKY_VERSION)));
     }
 
-    /**
-     * 打印Logo
-     */
-    public static void printLogo() {
-//        try {
-//            Console.println(read(resource(LUCKY_LOGO_FILE)), FontUtil.getWhiteUnderline(LUCKY_VERSION));
-//        } catch (IOException e) {
-//            // ignore
-//        }
-    }
 }
