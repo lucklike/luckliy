bf4e07f5a42ca2a5079b33643f5916163821f50a
fk7075 <1814375626@qq.com>
2026-01-31 04:16:28 +0800

    20260131 支持通过@ConvertMetaType注解手动指定响应类型

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/core/meta/ContentType.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/core/meta/ContentType.java
@@ -18,6 +18,11 @@ import java.util.Map;
  */
 public final class ContentType implements Serializable {
 
+    public static final String JSON = "application/json";
+    public static final String XML = "application/xml";
+    public static final String JAVA = "application/x-java-serialized-object";
+    public static final String PROTOBUF = "application/x-protobuf";
+
 
     // constants
     public static final ContentType APPLICATION_JSON = create(MimeTypeUtils.APPLICATION_JSON);
