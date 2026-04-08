56d7382d334befeedd3181aed381b4ceeb5de69c
fk7075 <1814375626@qq.com>
2026-04-09 00:39:40 +0800

    20260409 新增注解@ConfigurableRecordReplay

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/mock/ConfigurableRecordReplay.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/mock/ConfigurableRecordReplay.java
@@ -38,7 +38,7 @@ import java.lang.annotation.Target;
 public @interface ConfigurableRecordReplay {
 
     /**
-     * 使用 SpEL 表达式返回一个{@link RecordReplay.Configuration}对象
+     * 使用 SpEL 表达式返回一个{@link ConfigurableRecordReplay.Configuration}对象
      */
     String value();
 
@@ -48,16 +48,262 @@ public @interface ConfigurableRecordReplay {
     class Functions {
 
         /**
-         * 计算 SpEL 表达式，返回一个{@link RecordReplay.Configuration}对象
+         * 计算 SpEL 表达式，返回一个{@link ConfigurableRecordReplay.Configuration}对象
          *
          * @param context 上下文对象
-         * @return {@link RecordReplay.Configuration}对象
+         * @return {@link ConfigurableRecordReplay.Configuration}对象
          */
         @FunctionAlias("record_replay_config")
-        public static RecordReplay.Configuration recordReplayConfig(Context context) {
+        public static Configuration recordReplayConfig(Context context) {
             ConfigurableRecordReplay ann = context.getMergedAnnotationCheckParent(ConfigurableRecordReplay.class);
-            return context.parseExpression(ann.value(), RecordReplay.Configuration.class);
+            return context.parseExpression(ann.value(), Configuration.class);
+        }
+    }
+
+    /**
+     * 记录不匹配时的策略
+     */
+    enum MismatchStrategy {
+        /**
+         * 随机返回一条其他的
+         */
+        RANDOM_ONE,
+
+        /**
+         * 调用真实环境
+         */
+        USE_TARGET;
+    }
+
+    /**
+     * 录制模式
+     */
+    enum Model {
+        /**
+         * 录制模式
+         */
+        RECORD,
+
+        /**
+         * 回放模式
+         */
+        REPLAY,
+
+        /**
+         * 关闭录制回放
+         */
+        OFF;
+    }
+
+    /**
+     * 配置类
+     */
+    class Configuration {
+        /**
+         * 录制模式
+         */
+        private Model model = Model.OFF;
+
+        /**
+         * 回放时不匹配时的策略
+         */
+        private MismatchStrategy replayMismatchStrategy = MismatchStrategy.USE_TARGET;
+
+        /**
+         * 录制条件
+         */
+        private String recordConditions = "#{$contentLength$ < 1048576}";
+
+        /**
+         * 记录ID
+         */
+        private String recordId = "#{__args_to_string__($mc$)}";
+
+        /**
+         * 录制文件存放位置
+         */
+        private String recordDir = "#{T(System).getProperty('user.dir')}/@RecordReplay";
+
+        /**
+         * 方法ID
+         */
+        private String methodId = "#{$method$.getName()}";
+
+        /**
+         * 录制的最大数量
+         */
+        private Integer recordMaxCount = 10;
+
+        /**
+         * 指定异步任务的执行器（支持SpEL表达式）
+         */
+        private String recordExecutor = "";
+
+        /**
+         * 回放时是否模拟延时
+         */
+        private boolean replayDelayMock = false;
+
+        /**
+         * 获取录制模式
+         *
+         * @return 录制模式
+         */
+        public Model getModel() {
+            return model;
         }
 
+        /**
+         * 设置录制模式
+         *
+         * @param model 录制模式
+         */
+        public void setModel(Model model) {
+            this.model = model;
+        }
+
+        /**
+         * 获取回放时不匹配时的策略
+         *
+         * @return 回放时不匹配时的策略
+         */
+        public MismatchStrategy getReplayMismatchStrategy() {
+            return replayMismatchStrategy;
+        }
+
+        /**
+         * 回放时不匹配时的策略
+         *
+         * @param replayMismatchStrategy 回放时不匹配时的策略
+         */
+        public void setReplayMismatchStrategy(MismatchStrategy replayMismatchStrategy) {
+            this.replayMismatchStrategy = replayMismatchStrategy;
+        }
+
+        /**
+         * 获取录制条件
+         *
+         * @return 录制条件
+         */
+        public String getRecordConditions() {
+            return recordConditions;
+        }
+
+        /**
+         * 设置录制条件
+         *
+         * @param recordConditions 录制条件
+         */
+        public void setRecordConditions(String recordConditions) {
+            this.recordConditions = recordConditions;
+        }
+
+        /**
+         * 获取记录ID
+         *
+         * @return 记录ID
+         */
+        public String getRecordId() {
+            return recordId;
+        }
+
+        /**
+         * 设置记录ID
+         *
+         * @param recordId 记录ID
+         */
+        public void setRecordId(String recordId) {
+            this.recordId = recordId;
+        }
+
+        /**
+         * 获取录制文件存放位置
+         *
+         * @return 录制文件存放位置
+         */
+        public String getRecordDir() {
+            return recordDir;
+        }
+
+        /**
+         * 设置录制文件存放位置
+         *
+         * @param recordDir 录制文件存放位置
+         */
+        public void setRecordDir(String recordDir) {
+            this.recordDir = recordDir;
+        }
+
+        /**
+         * 获取方法ID
+         *
+         * @return 方法ID
+         */
+        public String getMethodId() {
+            return methodId;
+        }
+
+        /**
+         * 设置方法ID
+         *
+         * @param methodId 方法ID
+         */
+        public void setMethodId(String methodId) {
+            this.methodId = methodId;
+        }
+
+        /**
+         * 获取录制的最大数量
+         *
+         * @return 录制的最大数量
+         */
+        public Integer getRecordMaxCount() {
+            return recordMaxCount;
+        }
+
+        /**
+         * 设置录制的最大数量
+         *
+         * @param recordMaxCount 录制的最大数量
+         */
+        public void setRecordMaxCount(Integer recordMaxCount) {
+            this.recordMaxCount = recordMaxCount;
+        }
+
+        /**
+         * 获取指定异步任务的执行器
+         *
+         * @return 指定异步任务的执行器
+         */
+        public String getRecordExecutor() {
+            return recordExecutor;
+        }
+
+        /**
+         * 设置指定异步任务的执行器
+         *
+         * @param recordExecutor 指定异步任务的执行器
+         */
+        public void setRecordExecutor(String recordExecutor) {
+            this.recordExecutor = recordExecutor;
+        }
+
+        /**
+         * 获取回放时是否模拟延时
+         *
+         * @return 回放时是否模拟延时
+         */
+        public boolean isReplayDelayMock() {
+            return replayDelayMock;
+        }
+
+        /**
+         * 设置回放时是否模拟延时
+         *
+         * @param replayDelayMock 回放时是否模拟延时
+         */
+        public void setReplayDelayMock(boolean replayDelayMock) {
+            this.replayDelayMock = replayDelayMock;
+        }
     }
 }
