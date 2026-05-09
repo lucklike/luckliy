fe1acfb9d016981763900c2283fcf92cd96a48ec
fukang1163 <fukang1163@cairenhui.com>
2026-05-09 16:48:26 +0800

    20260508 异常优化

--- lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/handle/DefaultHttpExceptionHandle.java
+++ lucky-httpclient/src/main/java/com/luckyframework/httpclient/proxy/handle/DefaultHttpExceptionHandle.java
@@ -1,8 +1,12 @@
 package com.luckyframework.httpclient.proxy.handle;
 
+import com.luckyframework.common.FontUtil;
 import com.luckyframework.httpclient.core.meta.Request;
 import com.luckyframework.httpclient.proxy.context.MethodContext;
 import com.luckyframework.httpclient.proxy.convert.ActivelyThrownException;
+import com.luckyframework.reflect.MethodUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 默认的Http异常处理器，打印Request以及异常信息
@@ -13,8 +17,12 @@ import com.luckyframework.httpclient.proxy.convert.ActivelyThrownException;
  */
 public class DefaultHttpExceptionHandle implements HttpExceptionHandle {
 
+    private static final Logger logger = LoggerFactory.getLogger(DefaultHttpExceptionHandle.class);
+
     @Override
     public Object exceptionHandler(MethodContext methodContext, Request request, Throwable throwable) throws Throwable {
-        throw ActivelyThrownException.getRootCause(throwable);
+        Throwable rootCause = ActivelyThrownException.getRootCause(throwable);
+        logger.error("HTTP proxy method ['{}'] execution failed.", FontUtil.getBlueUnderline(MethodUtils.getLocation(methodContext.getCurrentAnnotatedElement())), rootCause);
+        throw rootCause;
     }
 }
